27a99ea9c6eb63a7f6a541cb9dc1daa98ebe778b
Thomas Basche <t0m8a5ch3@gmail.com>
2016-10-13 23:23:34 +1100

    Added user to book model

--- bookchook/models.py
+++ bookchook/models.py
@@ -4,8 +4,9 @@ from django.db import models
 from django.utils import timezone
 
 class Book(models.Model):
-    name = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
+    user = models.CharField(max_length=200, default="admin")
 
     def __str__(self):
         return self.name
