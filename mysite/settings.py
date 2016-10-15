8fc12ece2cb42b382bab931e312ae3ab4020b134
tombasche <t0m8a5ch3@gmail.com>
2016-10-15 13:49:15 +1100

    Switched to heroku postgres

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd70ipbpakogto1',
+        'USER': 'kwiesiveoztnao',
+        'PASSWORD': '',
+        'HOST': 'ec2-54-235-102-235.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
