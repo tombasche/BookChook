3ea8ff1e71f6ade11a7850a0943f2d7d6e5eea17
Tom Basche <tom.basche@hotmail.com>
2016-12-23 13:24:43 +1100

    Save form data as cookies (name / author) so it's remembered between sessions

--- bookchook/static/js/main.js
+++ bookchook/static/js/main.js
@@ -1,3 +1,21 @@
+
+
+
+function getEventListeners(){
+  var n = document.getElementById('id_name');
+  var a = document.getElementById('id_author');
+  n.addEventListener('blur', function() {
+    var name = document.getElementById('id_name').value;
+    console.log(name);
+    document.cookie = "name="+name;
+  });
+  a.addEventListener('blur', function() {
+    var author = document.getElementById('id_author').value;
+    console.log(author);
+    document.cookie = "author="+author;
+  });
+}
+
 function displaySeriesNumber() {
   var s = document.getElementById("id_series");
   var name = s.options[s.selectedIndex].text;
@@ -17,7 +35,29 @@ function displaySeriesNumber() {
   }
 }
 
-document.onkeyup=function(e){
+function getCookie(name) {
+  var re = new RegExp(name + "=([^;]+)");
+  var value = re.exec(document.cookie);
+  return (value != null) ? unescape(value[1]) : null;
+}
+
+function getFormValues() {
+  document.getElementById("id_name").value = getCookie("name");
+  document.getElementById("id_author").value = getCookie("author");
+}
+
+function deleteCookie(name) {
+    document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+};
+
+function wipeCookies() {
+
+  deleteCookie('name');
+  deleteCookie('author');
+
+}
+
+document.onkeyup=function(e) {
   var e = e || window.event; // for IE to cover IEs window event-object
   // + to add a book
   if (e.which == '65' && document.getElementsByClassName('book-form').length == 0) {
