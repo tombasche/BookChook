6f3b9bf305c78e8de1ac01c8a83756631015f75f
Thomas Basche <t0m8a5ch3@gmail.com>
2017-01-28 20:44:48 +1100

    Added docs

--- bookchook/static/js/main.js
+++ bookchook/static/js/main.js
@@ -1,6 +1,7 @@
-
-
-
+/**
+ * getEventListeners adds event listeners on blur to name and author fields
+ * should they exist (on the book form)
+ */
 function getEventListeners(){
   var n = document.getElementById('id_name');
   var a = document.getElementById('id_author');
@@ -20,6 +21,9 @@ function getEventListeners(){
   }
 }
 
+/* displaySeriesNumber hides the series number if a series has
+ * not been selected on the book form
+ */
 function displaySeriesNumber() {
   var s = document.getElementById("id_series");
   var name = s.options[s.selectedIndex].text;
@@ -39,12 +43,20 @@ function displaySeriesNumber() {
   }
 }
 
+/* getCookie returns a cookie by name
+ *
+ * @param name - name of the cookie
+ * @returns the value of that cookie
+ */
 function getCookie(name) {
   var re = new RegExp(name + "=([^;]+)");
   var value = re.exec(document.cookie);
   return (value != null) ? unescape(value[1]) : null;
 }
 
+/* getFormValues returns the values stored in cookies for
+ * the book form.
+ */
 function getFormValues() {
 
   var name = document.getElementById("id_name");
@@ -63,20 +75,24 @@ function getFormValues() {
   }
 }
 
+/* deleteCookie deletes a cookie stored for the session
+ *
+ * @param name - name of the cookie to delete
+ */
 function deleteCookie(name) {
     document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 };
 
+/* wipeCookies is a wrapper function to wipe name and author cookies
+ */
 function wipeCookies() {
-
   deleteCookie('name');
   deleteCookie('author');
-
 }
 
+// pressing 'a' while on the book list screen will add a new book
 document.onkeyup=function(e) {
   var e = e || window.event; // for IE to cover IEs window event-object
-  // + to add a book
   if (e.which == '65' && document.getElementsByClassName('book-table').length > 0) {
     document.getElementById('add-book').click();
   }
