ee9b562aa2159885d3849d1492d5ac22d039fbe4
Tom Basche <tcbasche@gmail.com>
2019-03-27 15:24:24 +1100

    Removed some guff for the fixening

--- bookchook/urls.py
+++ bookchook/urls.py
@@ -1,12 +1,2 @@
-from django.conf.urls import include
-from django.urls import path
-from rest_framework import routers
-from bookchook import views
-
-router = routers.DefaultRouter()
-router.register(r'books', views.BookViewSet)
-router.register(r'series', views.SeriesViewSet)
-
 urlpatterns = [
-    path('', include(router.urls))
 ]
--- bookchook_site/urls.py
+++ bookchook_site/urls.py
@@ -3,13 +3,9 @@ import django.contrib.auth.views
 
 from django.contrib import admin
 from django.urls import path
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 urlpatterns = [
     path(r'', admin.site.urls),
     path(r'accounts/login/', django.contrib.auth.views.LoginView, name='login'),
     path(r'jet/', include('jet.urls', 'jet')),  # Django JET URLS
-    path('api/', include('bookchook.urls')),
-    path('api/login/', obtain_jwt_token),
-    path('api/refresh/', refresh_jwt_token)
 ]
